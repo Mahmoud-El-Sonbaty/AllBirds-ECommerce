0f8260d703dfd55c230345358faf274bb97e7dee
Kareem <mahmoudelsonbaty777@gmail.com>
2024-10-19 00:17:04 +0300

    remove second category seeding

--- AllBirds.AdminDashboard/Controllers/ProductController.cs
+++ AllBirds.AdminDashboard/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 
-﻿using AllBirds.Application.Services.ProductServices;
+using AllBirds.Application.Services.CategoryServices;
+using AllBirds.Application.Services.ProductServices;
 using AllBirds.DTOs.CategoryDTOs;
 using AllBirds.DTOs.ProductDTOs;
 using Microsoft.AspNetCore.Mvc;
@@ -11,12 +12,12 @@ namespace AllBirds.AdminDashboard.Controllers
     public class ProductController : Controller
     {
         private readonly IProductService productservice;
-        //private readonly ICategoryService categoryservice;
+        private readonly ICategoryService categoryservice;
 
-        public ProductController(IProductService _productService )
+        public ProductController(IProductService _productService, ICategoryService _categoryService)
         {
             productservice = _productService;
-            //categoryservice = _categoryService;
+            categoryservice = _categoryService;
         }
         public IActionResult Index()
         {
@@ -34,15 +35,8 @@ namespace AllBirds.AdminDashboard.Controllers
         [HttpGet]
         public async Task<IActionResult> Create()
         {
-            ViewBag.Categories = new List<CUCategoryDTO>()
-            {
-                new() { Id = 1, NameAr = "رجالي", NameEn = "Men" },
-                new() { Id = 2, NameAr = "نسائي", NameEn = "Women" },
-                new() { Id = 3, NameAr = "أحذية رجالي", NameEn = "Men Shoes" },
-                new() { Id = 4, NameAr = "أحذية نشطة رجالي", NameEn = "Men Active Shoes" },
-                new() { Id = 5, NameAr = "أحذية نسائية", NameEn = "Women Shoes" },
-                new() { Id = 6, NameAr = "أحذية رياضية يومية نسائية", NameEn = "Women Every Day Sneakers" }
-            };
+            List<GetAllCategoryDTO> allCategoryDTOs = await categoryservice.GetAllAsync();
+            ViewBag.Categories = allCategoryDTOs;
             return View();
         }
         
@@ -60,15 +54,8 @@ namespace AllBirds.AdminDashboard.Controllers
         [HttpGet]
         public async Task<IActionResult> Update(int id)
         {
-            List<CUCategoryDTO> cUCategoryDTOs = new List<CUCategoryDTO>()
-            { 
-                new CUCategoryDTO() { Id = 1 , NameAr = " قسم 1", NameEn = "Category 1" },
-                new CUCategoryDTO() { Id = 2 , NameAr = " قسم 2", NameEn = "Category 2" },
-                new CUCategoryDTO() { Id = 3 , NameAr = " قسم 3", NameEn = "Category 3" },
-                new CUCategoryDTO() { Id = 4 , NameAr = " قسم 4", NameEn = "Category 4" },
-                new CUCategoryDTO() { Id = 5 , NameAr = " قسم 5", NameEn = "Category 5" } 
-            };
-            ViewBag.Categories = cUCategoryDTOs;
+            List<GetAllCategoryDTO> allCategoryDTOs = await categoryservice.GetAllAsync();
+            ViewBag.Categories = allCategoryDTOs;
             var Prd = await productservice.GetByIdAsync(id);
             return View(Prd);
         }
@@ -88,20 +75,19 @@ namespace AllBirds.AdminDashboard.Controllers
             }
             return View();
         }
-            public async Task<IActionResult> Delete(int id)
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id > 0)
             {
-                if (id > 0)
-                {
-                    CUProductDTO deletedProduct = await productservice.SoftDeleteAsync(id);
+                CUProductDTO deletedProduct = await productservice.SoftDeleteAsync(id);
                 if (deletedProduct is not null)
                 {
                     return RedirectToAction("GetAll");
-
                 }
-                else
-                    return View();
-                }
-                    return View();
             }
+            return null;
         }
+    }
 }
